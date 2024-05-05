b2d3421f545e1454c43ebfd24679182155387ba2
JoseTorres <joseangel.tosa@outlook.com>
2024-05-05 01:25:24 -0700

    Metodo Imprimir, OK

--- AppEmpleadoOvertime/src/appempleadoovertime/AppEmpleadoOvertime.java
+++ AppEmpleadoOvertime/src/appempleadoovertime/AppEmpleadoOvertime.java
@@ -4,11 +4,41 @@
  */
 package appempleadoovertime;
 
+import java.util.*;
+
 /**
  *
  * @author Jose
  */
 public class AppEmpleadoOvertime {
+
+    private Scanner teclado;
+    private String nombre;
+    private String apellido;
+    private String idEmpleado;
+    private int hrTrabajadas;
+
+    //Ingreso de datos
+    public void inicializar() {
+        teclado = new Scanner(System.in);
+        System.out.print("Nombre del empleado: ");
+        nombre = teclado.next();
+        System.out.print("IApellido del empleado: ");
+        apellido = teclado.next();
+        System.out.println("Id del empleado");
+        idEmpleado = teclado.next();
+        System.out.println("¿Cuantas horas trabajo este empleado?");
+        hrTrabajadas = teclado.nextInt();
+    }
+
+    // Mostrar daot sdel empleado
+    public void imprimir() {
+        System.out.println("\t--- Datos del empleado ---");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Apellido: " + apellido);
+        System.out.println("Id. empleado: " + idEmpleado);
+        System.out.println("Horas trabajadas: " + hrTrabajadas);
+    }
 
     /**
      * @param args the command line arguments
@@ -16,5 +46,5 @@ public class AppEmpleadoOvertime {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
 }
